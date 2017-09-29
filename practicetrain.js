3e3fbdba714b21924e22382b2a0be967ffd4ee46
Karina Chan <karinachan902@gmail.com>
2017-09-29 09:59:12 -0700

    new code

---
+++ practicetrain.js
@@ -0,0 +1,96 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB6h3Zkgqh7NRM-SZyKdiz_P4X8o_KTDbM",
+    authDomain: "karina-firebase-app.firebaseapp.com",
+    databaseURL: "https://karina-firebase-app.firebaseio.com",
+    projectId: "karina-firebase-app",
+    storageBucket: "karina-firebase-app.appspot.com",
+    messagingSenderId: "621536385175"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.databse();
+
+$('addTrainBtn').on("click", function(){
+
+	var trainName = $('#trainNameInput').val().trim();
+	var destination = $('#destInput').val().trim();
+	var firstTrain = $('#firstTrainInput').val().trim();
+	var frequency = $('freqInput').val().trim();
+
+
+	var newTrain ={
+		name: trainName,
+		dest: destination,
+		first: firstTrain,
+		freq: frequency
+	} 
+
+	database.ref().push(newTrain);
+
+	console.log(newTrain.name);
+	console.log(newTrain.dest);
+	console.log(newTrain.first);
+	console.log(newTrain.freq);
+
+	$('#trainNameInput').val("");
+	$('#destInput').val("");
+	$('#firstTrainInput').val("");
+	$('#freqInput').val("");
+
+	return false;
+
+});
+
+database.ref().on("child_added", function(childSnapshot){
+	console.log(childSnapshot.val());
+
+	var trainName = childSnapshot.val().name;
+	var destination = childSnapshot.val().dest;
+	var firstTrain = childSnapshot.val().first;
+	var frequency = childSnapshot.val().freq;
+
+
+	console.log(trainName);
+	console.log(destination);
+	console.log(firstTrain);
+	console.log(frequency);
+
+
+	var firstTimeConverted = moment(firstTrain, "hh:mm").subtract(1, "years");
+	console.log(firstTimeConverted);
+
+	var currentTime = moment();
+	console.log("CURRENT TIME:" + moment(currentTime).format("HH:mm"));
+
+	var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+	console.log("DIFFERENCE IN TIME:" + diffTime);
+
+	var tRemainder = diffTime % frequency;
+	console.log(tRemainder);
+
+	var tMinutesTillTrain = frequency = tRemainder;
+	console.log("ARRIVAL TIME:" + moment(nextTrain).format("hh:mm"));
+
+
+
+
+	$("#trainTable > tbody").append("<tr><td>" + trainName + "</td><td>" + destination  + "</td><td>" + frequency + "</td><td>" + nextTrain + "</td><td>" + tMinutesTillTrain + "</td></tr>");
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
